b53546edff619d2e06a6eca2cad70804434d4b8b
SaiKumar2806 <saikumarkagula333@gmail.com>
2025-04-10 21:04:42 +0530

    Fix Netlify deploy: add _redirects and install missing dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
